60d3e3bc47e36d2c8a330be67a9a629ce446e2e7
stevenadrien <stevenadrien@yahoo.com>
2018-07-20 17:46:56 -0500

    Update ParkingPass.h

--- ParkingPass.h
+++ ParkingPass.h
@@ -11,16 +11,52 @@
 
 class ParkingPass
 {
+    
 private:
     char parkingPassType;
     int parkingSpot;
     
+public:
+    
+    ParkingPass();
+    
+    char getParkingPassType()
+    {
+        return parkingPassType;
+    }
+    
+    void setParkingPassType(char pass)
+    {
+        parkingPassType = pass;
+    }
+    
+    int getParkingSpot()
+    {
+        return parkingSpot;
+    }
+    
+    void setParkingSpot(int spotNumber)
+    {
+        parkingSpot = spotNumber;
+    }
+    
+    
+    void setParkingPassInfo()
+    {
+        cout<<"Enter parking pass type (p for premium, b for basic, d for day pass): "<<endl;
+        cin>>parkingPassType;
+        
+        if(parkingPassType == 'p')
+        {
+            cout<<"Enter customer's reserved parking spot number (from 1-100): "<<endl;
+            cin>>parkingSpot;
+        }
+        
+    }
     
     
     
-    
-    
-}
+};
 
 
 #endif /* ParkingPass_h */

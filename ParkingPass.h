bb6f0b70242bb0967914ab1ff2eb220dcecb2126
stevenadrien <stevenadrien@yahoo.com>
2018-07-29 23:24:51 -0500

    Update ParkingPass.h

--- ParkingPass.h
+++ ParkingPass.h
@@ -61,15 +61,26 @@ public:
         duration = dur;
     }
     
-   
+    
+    
+    //functions to do stuff manually (like passing parking pass data for testing purposes ;))
+    void setParkingPassData(char parkingType)
+    {
+        setParkingPassData(parkingType);
+    }
+    
+    void setParkingPassData(char parkingType, int parkingSpot)
+    {
+        setParkingPassData(parkingType);
+        setParkingSpot(parkingSpot);
+    }
+    
     
     
     
     
     //"Advanced" functions (related to customer issues; valid passes//setting parking spot info;; typically
     //essentially ties into some higher level function in a "parent" class
-    
-    
 
     
    /* bool isItExpired()

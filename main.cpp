258c5defced3a2b0253aa0fcaa2c0bdb09b3b4e7
stevenadrien <stevenadrien@yahoo.com>
2018-08-03 07:47:08 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -37,41 +37,73 @@ int main()
     do
     {
         cout << "Please type in your desired action. Type in -1 to logout and exit the program:"<<endl;
-        cout << "1. Issue A New Parking Pass"<<endl;
-        cout << "2. Renew A Parking Pass"<<endl;
+        cout << "1. Park A Car"<<endl;
+        cout << "2. Un-Park A Car"<<endl;
         cout << "3. Set Parking Pass Prices"<<endl;
-        cout << "4. Display Business Analytics"<<endl;
+        cout << "4. Display Operations Analytics"<<endl;
+        cout << "5. Display Business Analytics"<<endl;
+        cout << "6. Issue A New Parking Pass"<<endl;
+        cout << "7. Renew A Parking Pass"<<endl;
+        cout << "8. Edit Customer Info"<<endl;
         
         cin >> userChoice;
+        int customerID;
+
         
     switch(userChoice)
         {
         
             case 1:
-                parkingGarage.addACustomer();
+                cout<<"Please enter the customer ID of the person desiring to park their car: "<<endl;
+                cin>>customerID;
+                parkingGarage.parkACar(customerID);
                 break;
+                
         
             case 2:
-                int customerID;
+                cout<<"Please enter the customer ID of the person desiring to park their car: "<<endl;
+                cin>>customerID;
+                parkingGarage.unParkACar(customerID);
+                
+            case 3:
+                parkingGarage.setParkingPassPrices();
+                break;
+        
+                
+            case 4:
+                parkingGarage.displayOperationsAnalytics();
+                break;
+ 
+                
+            case 5:
+                parkingGarage.displayBusinessAnalytics();
+                break;
+                
+                
+            case 6:
+                parkingGarage.addACustomer();
+                break;
+            
+                
+            case 7:
                 cout<<"Please enter the customer ID of the person desiring to renew their parking pass: "<<endl;
                 cin>>customerID;
                 
                 parkingGarage.renewParkingPass(customerID);
                 break;
-        
-            case 3:
-                parkingGarage.showCurrentPassPrices();
-                break;
-        
-            case 4:
-                parkingGarage.displayBusinessAnalytics();
-                break;
+                
+                
+            case 8:
+                cout<<"Pleas enter the customer ID of the person desiring to edit their information: "<<endl;
+                cin>>customerID;
+                parkingGarage.editCustomerInfo(customerID);
+                
         }
     
         
     } while(userChoice != -1);
     
-    
+    /*
     
     string fName, lName, pNumber;
     parkingGarage.addACustomer();
@@ -86,11 +118,12 @@ int main()
     
     parkingGarage.parkACar(customer1);
     
-    parkingGarage.displayOperationsInformation();
+    parkingGarage.displayOperationsAnalytics();
     
     
     
     parkingGarage.addACustomer(customer1);
+     */
     
     
     

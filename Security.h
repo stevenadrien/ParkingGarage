2e6b1034b427b634a9e22358b4d05e1894950372
Steven Ouandji <stevenadrien@yahoo.com>
2018-08-08 05:23:17 -0500

    Update Security.h

--- Security.h
+++ Security.h
@@ -1,3 +1,19 @@
+
+//
+//  Security.h
+//  ParkingGarage
+//
+//  Created by Steven Ouandji on 26/07/18.
+//  Copyright © 2018 Steven Ouandji. All rights reserved.
+//
+
+/*This class deals with the security and overall safeguard protocols for the parking garage system.  Things like; authentication, data fuzzing, encryption, etc will be implemented here.
+ 
+ 
+ */
+
+
+
 #ifndef Security_h
 #define Security_h
 
